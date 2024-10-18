790218bcad6cf29e6bf49027b955a40689444dc1
vignesh <y.leelavignesh@gmail.com>
2024-10-18 08:43:36 +0530

    terraform

---
+++ ec2.tf
@@ -0,0 +1,34 @@
+resource "aws_instance" "sample" {
+  #count = 3
+  ami = "ami-06b21ccaeff8cd686"
+  instance_type = "t3.micro"
+  vpc_security_group_ids = [aws_security_group.allow-ssh.id]
+  
+  tags = {
+    type = "map"
+    Name = "sample"
+  }
+}
+
+resource "aws_security_group" "allow-ssh" {
+  # ... other configuration ...
+  
+  ingress {
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+  tags = {
+    type = "map"
+    Name = "allow-ssh"
+  }
+}
